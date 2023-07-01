1a1accc0cd11b53389c374ffd692550dac94adcc
mayurjadhav2002 <mayurshrikatjadhav@gmail.com>
2023-07-01 16:33:17 +0530

    update v1.0.7.1-Mayur Jadhav

--- src/Components/Blogs/Blog.js
+++ src/Components/Blogs/Blog.js
@@ -19,9 +19,9 @@ function Blog() {
 
             <div className="w-full mx-auto">
 
-              <h1 className='text-5xl font-bold my-2'>A small headline to switch your visitors into users. </h1>
-              <div className='flex justify-between my-5'>
-                <div className="flex items-center space-x-4">
+              <h1 className='lg:text-5xl text-4xl font-bold my-2'>A small headline to switch your visitors into users. </h1>
+              <div className='flex justify-between my-5 items-center'>
+                <div className="flex items-center space-x-1 lg:space-x-3">
                   <img
                     className="w-10 h-10 rounded-full"
                     src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
@@ -39,23 +39,9 @@ function Blog() {
 
                 {/*User Social Media*/}
 
-                <div className="  max-w-7xl md:flex md:items-center md:justify-between ">
-                  <div className="flex justify-center mb-8 space-x-6 md:order-last md:mb-0">
-                    <Link to="https://facebook.com" target='blank' className="text-blue-700 hover:text-blue-800">
-                      <span className="sr-only">Facebook</span>
-                      <svg
-                        className="w-6 h-6"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </Link>
+                <div className=" items-center max-w-7xl md:flex md:items-center md:justify-between ">
+                  <div className="flex items-center justify-center mb-8 space-x-2 md:order-last md:mb-0">
+                    
                     <Link to="/" target='blank' className="text-pink-400 hover:text-pink-500">
                       <span className="sr-only">Instagram</span>
                       <svg
@@ -71,17 +57,7 @@ function Blog() {
                         />
                       </svg>
                     </Link>
-                    <Link to="/" target='blank' className="text-cyan-400 hover:text-cyan-500">
-                      <span className="sr-only">Twitter</span>
-                      <svg
-                        className="w-6 h-6"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        aria-hidden="true"
-                      >
-                        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                      </svg>
-                    </Link>
+                 
                     <Link to="/" target='blank' className="text-gray-400 hover:text-gray-500">
                       <span className="sr-only">GitHub</span>
                       <svg
--- src/Components/Company/Errors.js
+++ src/Components/Company/Errors.js
@@ -3,12 +3,12 @@ import ErrorImg from '../../Assets/SVG/error.svg'
 import './Errors.css'
 function Errors() {
   return (
-    <div className='w-3/4 mx-auto  p-10 justify-center items-center'>
+    <div className='lg:w-3/4 w-full mx-auto p-2  lg:p-10 justify-center items-center'>
       <img src={ErrorImg} alt="" className='mx-auto'/>
-      <div className='w-4/5 mx-auto my-10 font-mono p-10  page-error text-gray-400'>
+      <div className='w-4/5  mx-auto lg:my-10 my-5 font-mono lg:p-10 py-2 px-1 page-error text-gray-400'>
             
-      <h1 className='text-center '>Hey you! I've Stole this Page. </h1>
-      <h2 className='text-end '>~ Arnim Zola</h2>
+      <h1 className='text-center lg:text-3xl p-none text-sm '>Hey you! I've Stole this Page. </h1>
+      <h2 className='text-end lg:text-2xl  text-sm'>~ Arnim Zola</h2>
       </div>
 
     </div>
